e8f68e4b7ab6dc370729a1be297d87c530eed8d5
madara123pain <161628269+madara123pain@users.noreply.github.com>
2025-10-05 08:44:06 +0500

    minor tweak

--- mpv/.config/mpv/scripts/clean_url.lua
+++
@@ -1,9 +0,0 @@
-mp.register_event("start-file", function()
-    local path = mp.get_property("path")
-    if path and path:match("^https?://") then
-        local cleaned = path:gsub("([?&])feature=shared", "")
-        if cleaned ~= path then
-            mp.set_property("path", cleaned)
-        end
-    end
-end)
