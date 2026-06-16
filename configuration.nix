1aded93ac6e2db7b81551b3428eeb554fefe3f37
Umer-Arif <omerarifking@gmail.com>
2026-06-16 22:08:57 +0500

    Migrate to localized home manager flake setup

---
+++ configuration.nix
@@ -0,0 +1,138 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Essential kernel & hardware configurations for Kanata to function
+  boot.kernelModules = [ "uinput" ];
+  hardware.uinput.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true; # Enable networking
+
+  # Set your time zone.
+  time.timeZone = "Asia/Karachi";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system & KDE Plasma Desktop
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."sama" = {
+    isNormalUser = true;
+    description = "omer";
+    group = "sama";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  users.groups.sama = {};
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    ghostty
+    mpv
+    kdePackages.kdenlive
+    rclone
+    syncthing
+    fastfetch
+    aria2
+    yt-dlp
+    emacs
+    stow
+  ];
+
+  # Fonts configuration
+  fonts = {
+    packages = with pkgs; [
+      iosevka
+    ];
+    fontconfig.defaultFonts = {
+      monospace = [ "Iosevka" ];
+    };
+  };
+
+  # Kanata service setup (with the process-unmapped-keys fix inside defcfg)
+  # Kanata service setup
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      default = {
+        devices = [ ];
+        config = ''
+          (defsrc
+            caps
+          )
+          (defalias
+            capsec (tap-hold 200 200 esc lctl)
+          )
+          (deflayer default
+            @capsec
+          )
+        '';
+      };
+    };
+  };
+
+  # System baseline settings and Flake features
+  system.stateVersion = "26.05";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Omer's Unstable NixOS Flake Configuration";
+
+  inputs = {
+    # Uses the absolute latest bleeding-edge packages
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Track the matching unstable branch for Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      # Matches your computer's hostname
+      sama = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+
+          # Inject Home Manager module directly into the system build
+          # Inject Home Manager module directly into the system build
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # Map Home Manager configuration strictly to your REAL username
+            home-manager.users.sama = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
